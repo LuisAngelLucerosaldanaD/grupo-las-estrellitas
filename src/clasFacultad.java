74967d97a33e19dd16cc0022f017d9ba308778cd
luis <luis.com>
2019-09-09 09:20:42 -0500

    master angel

---
+++ src/clasFacultad.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Loloy
+ */
+public class clasFacultad {
+    public static void main(String[] args) {
+        guis ok=new guis();
+        ok.setVisible(true);
+    }
+}
---
+++ src/guis.java
@@ -0,0 +1,18 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+
+/**
+ *
+ * @author Loloy
+ */
+public class guis extends JFrame implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        
+    }
+    
+}
